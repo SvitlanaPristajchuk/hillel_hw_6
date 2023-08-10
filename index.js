76e5a4f52863ab985d7051a88f23f496f491abac
Svitlana Pristajchuk <svetlana.pristajchuk@gmail.com>
2023-08-10 16:05:49 +0200

    add

---
+++ index.js
@@ -0,0 +1,37 @@
+hero = ['Ivan'];
+native = ['York', 'Of'];
+destination = ['Poltava', 'In'];
+
+//Об'єднати масиви destination, native, hero в спільний масив rainbow.
+//rainbow = [...destination, ...native, ...hero]
+
+rainbow = destination.concat(native, hero)
+console.log(rainbow)
+
+//Зробити реверс елементів отриманого масиву rainbow.
+newRainbow = rainbow.reverse()
+console.log(newRainbow) // ['Ivan', 'of', 'York', 'in', 'Poltava' ]
+
+//Змінити/додати елементи масиву rainbow так, щоб в результаті отримати масив вигляду ['Richard','Of','York','Gave','Battle','In','Vain'];
+
+newRainbow = newRainbow.slice(1, 4)
+console.log(newRainbow)
+
+newRainbow.push('Vain')
+console.log(newRainbow)
+newRainbow.unshift('Richard')
+
+console.log(newRainbow) //'richard' 'of', 'york', 'in 'vain'
+
+newRainbow.splice(3, 0, 'Gave', 'Battle')
+console.log(newRainbow) //['Richard','Of','York','Gave','Battle','In','Vain']
+
+arr = JSON.parse(JSON.stringify(newRainbow));
+
+
+
+rainList = newRainbow.join(`</li><li>`);
+
+
+document.write(`<ul>
+   <li>${rainList}</li></ul>`);
